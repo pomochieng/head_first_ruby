04b91404632863e6a1de27be6edfa236c372341c
pomochieng <pomochieng@gmail.com>
2016-09-28 18:53:20 +0000

    add move and report age methods

--- chapter_2/instancevariableanimal.rb
+++ chapter_2/instancevariableanimal.rb
@@ -6,9 +6,24 @@ class Dog
   def talk
     puts "#{@name} says Bark!"     #Access the instance variable.
   end
+
+  def move(destination)
+    puts "#{@name} runs to the #{destination}."
+  end
+
+  def make_up_age
+    @age = 5
+  end
+
+  def report_age
+    puts "#{@name} is #{@age} years old"
+  end
+
 end
 
 dog = Dog.new
 dog.make_up_name
-dog.talk
+dog.move("yard")
+dog.make_up_age
+dog.report_age
 
