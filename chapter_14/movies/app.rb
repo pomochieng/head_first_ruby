4f023e8f370a5ce1a9fce5cb87834c6297b9e583
pomochieng <pomochieng@gmail.com>
2016-10-06 12:38:06 +0000

    embedding a movie title in our html

--- chapter_14/movies/app.rb
+++ chapter_14/movies/app.rb
@@ -3,6 +3,6 @@ require 'movie'
 
 get('/movies') do  #Responds to GET requests for "/movies".
   @movie = Movie.new   #Set up a new Movie object
-  @movie.title
+  @movie.title = "Jaws"
   erb :index       #Load "views/index.erb"
 end
