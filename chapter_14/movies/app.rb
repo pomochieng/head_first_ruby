454207b38e0483f33d01e9a197741ade797749ff
pomochieng <pomochieng@gmail.com>
2016-10-06 11:06:03 +0000

    setting up a movie object in the Sinatrs app

--- chapter_14/movies/app.rb
+++ chapter_14/movies/app.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
+require 'movie'
 
 get('/movies') do  #Responds to GET requests for "/movies".
+  @movie = Movie.new   #Set up a new Movie object
+  @movie.title
   erb :index       #Load "views/index.erb"
 end
