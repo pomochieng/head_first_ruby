db9bb4a48007a8fb5c7666dad2a194f4d14385dc
pomochieng <pomochieng@gmail.com>
2016-10-09 13:43:40 +0000

    using embedded tags to add putput for each item in an array

--- chapter_14/movies/app.rb
+++ chapter_14/movies/app.rb
@@ -2,7 +2,12 @@ require 'sinatra'
 require 'movie'
 
 get('/movies') do  #Responds to GET requests for "/movies".
-  @movies = Movie.new   #Set up a new Movie object
-  @movies.title = "Jaws"
+  @movies = []     #Set up an array of movies
+  @movies[0] = Movie.new   #Set up a new Movie object
+  @movies[0].title = "Jaws"
+  @movies[1] = Movie.new
+  @movies[1].title = "Alien"
+  @movies[2] = Movie.new
+  @movies[2].title = "Terminator 2"
   erb :index       #Load "views/index.erb"
 end
