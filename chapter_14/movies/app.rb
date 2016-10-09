771cd8dbdb173248d62f775979c506afc8e57f2f
pomochieng <pomochieng@gmail.com>
2016-10-09 12:51:51 +0000

    a regular embedding tag

--- chapter_14/movies/app.rb
+++ chapter_14/movies/app.rb
@@ -2,12 +2,7 @@ require 'sinatra'
 require 'movie'
 
 get('/movies') do  #Responds to GET requests for "/movies".
-  @movies = []
-  @movies[0] = Movie.new   #Set up a new Movie object
-  @movies[0].title[0] = "Jaws"
-  @movies[1] = Movie.new
-  @movies[1].title = "Alien"
-  @movies[2] = Movie.new
-  @movies[2].title = "Terminator 2"
+  @movies = Movie.new   #Set up a new Movie object
+  @movies.title = "Jaws"
   erb :index       #Load "views/index.erb"
 end
